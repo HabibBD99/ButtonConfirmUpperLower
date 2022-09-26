76fc34964c00de876159bc6fd3017a2ab569d7bb
Habib <70913270+HabibBD99@users.noreply.github.com>
2022-09-26 22:50:09 +0600

    ButtonGonfirmUpperLower
    html css js

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+function CapitalizeCase() {
+ if(Capitalize==Case){
+  confirm("Do You Want To Capitalize?");
+ }
+var Capitalize = document.getElementById('Text');
+var Case = Capitalize.style.textTransform="capitalize";
+
+}
+
+
+
+
+
+function UpperCase() {
+ if(Upper==Case){
+  confirm("Do You Want to Uppercase?");
+ }
+ var Upper = document.getElementById('Text');
+ var Case = Upper.style.textTransform="uppercase";
+
+}
+
+
+
+
+
+
+function LowerCase() {
+ if(Lower==Case){
+  confirm("Do You Want to Lowercase?");
+ }
+var Lower = document.getElementById('Text');
+var Case =  Lower.style.textTransform="lowercase";
+         
+}
